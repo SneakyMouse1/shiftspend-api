975e422c92795a8c471baf28b79115c3de6cf069
Semyon Smyslov <smyslovich@gmail.com>
2026-07-27 09:40:22 +0200

    build(deps): add excel/dompdf packages and central threshold configs
    Co-authored-by: Tati <teana318san@gmail.com>

---
+++ config/reports.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'export_sync_limit' => (int) env('EXPORT_SYNC_LIMIT', config('thresholds.async_processing_limit', 500)),
+    'export_ttl_hours'  => (int) env('EXPORT_TTL_HOURS', 2),
+    'storage_disk'      => env('EXPORT_STORAGE_DISK', 'local'),
+    'max_period_days'   => (int) env('EXPORT_MAX_PERIOD_DAYS', 366),
+    'allowed_periods'   => ['last_month', 'previous_month', '3_months', '6_months', '1_year', 'custom'],
+];
---
+++ config/thresholds.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'async_processing_limit' => (int) env('ASYNC_PROCESSING_LIMIT', 500),
+];
